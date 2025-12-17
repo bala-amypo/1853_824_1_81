4bd89eaf69c6bb15ce39e252baf8ce0d15e17059
bala-amypo <bala@amypo.in>
2025-12-17 06:48:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/Assetentity.java
+++ src/main/java/com/example/demo/entity/Assetentity.java
@@ -2,5 +2,8 @@ package com.example.demo.repository;
 
 public class Assetentity  {
     private Long Id
-    private String assetTag
+    private String assetTag;
+    private String assetType;
+    private String model;
+    
 }
