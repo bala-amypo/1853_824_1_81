4ae3de227762bcaa52917abeab607606b5981ed4
bala-amypo <bala@amypo.in>
2025-12-17 08:24:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Assetentity.java
+++ src/main/java/com/example/demo/entity/Assetentity.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.repository;
 import jakarta.persistence.Entity;
 
@@ -11,5 +12,54 @@ public class Assetentity  {
     private String status;
     private User currentHolder;
     private LocalDateTime createdAt
+    public Long getId() {
+        return Id;
+    }
+    public void setId(Long id) {
+        Id = id;
+    }
+    public String getAssetTag() {
+        return assetTag;
+    }
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
+    }
+    public String getAssetType() {
+        return assetType;
+    }
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public LocalDate getPurchaseData() {
+        return PurchaseData;
+    }
+    public void setPurchaseData(LocalDate purchaseData) {
+        PurchaseData = purchaseData;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public User getCurrentHolder() {
+        return currentHolder;
+    }
+    public void setCurrentHolder(User currentHolder) {
+        this.currentHolder = currentHolder;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
     
+}
 }
