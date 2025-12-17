0332316d82e898bb065f3f6a1b2c9591793cd773
bala-amypo <bala@amypo.in>
2025-12-17 08:29:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/Assetentity.java
+++ src/main/java/com/example/demo/entity/Assetentity.java
@@ -1,65 +1,95 @@
-
 package com.example.demo.repository;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
-public class Assetentity  {
-    private Long Id
+public class AssetEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String assetTag;
     private String assetType;
     private String model;
-    private LocalDate PurchaseData ;
+    private LocalDate purchaseDate;
     private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "current_holder_id")
     private User currentHolder;
-    private LocalDateTime createdAt
+
+    private LocalDateTime createdAt;
+
     public Long getId() {
-        return Id;
+        return id;
     }
+
     public void setId(Long id) {
-        Id = id;
+        this.id = id;
     }
+
     public String getAssetTag() {
         return assetTag;
     }
+
     public void setAssetTag(String assetTag) {
         this.assetTag = assetTag;
     }
+
     public String getAssetType() {
         return assetType;
     }
+
     public void setAssetType(String assetType) {
         this.assetType = assetType;
     }
+
     public String getModel() {
         return model;
     }
+
     public void setModel(String model) {
         this.model = model;
     }
-    public LocalDate getPurchaseData() {
-        return PurchaseData;
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
     }
-    public void setPurchaseData(LocalDate purchaseData) {
-        PurchaseData = purchaseData;
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
     public User getCurrentHolder() {
         return currentHolder;
     }
+
     public void setCurrentHolder(User currentHolder) {
         this.currentHolder = currentHolder;
     }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
+
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
-    
-}
 }
