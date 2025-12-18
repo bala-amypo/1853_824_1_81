c959cd9b0179e5cd0ab4be843b500b54a9fe5c72
bala-amypo <bala@amypo.in>
2025-12-18 10:06:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/LifecycleEvententity.java
+++ src/main/java/com/example/demo/entity/LifecycleEvententity.java
@@ -1,11 +1,13 @@
 package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
 @Entity
-public class Lifecycleevententity {
+public class LifecycleEvententity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
