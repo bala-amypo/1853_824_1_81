57f0263100c6eff7e4c38a29a6962298929c0564
bala-amypo <bala@amypo.in>
2025-12-18 09:36:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/LifecycleEvententity.java
+++ src/main/java/com/example/demo/entity/LifecycleEvententity.java
@@ -2,50 +2,25 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
-
-import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "lifecycle_events")
 public class LifecycleEventEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "asset_id", nullable = false)
-    private AssetEntity asset;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "event_type", nullable = false)
-    private EventType eventType;
-
-    @Column(name = "event_description", nullable = false)
+    private String asset;
+    private String eventType;
     private String eventDescription;
+    private String eventDate;
+    private String performedBy;
 
-    @Column(name = "event_date", nullable = false)
-    private LocalDateTime eventDate;
-
-    @ManyToOne(optional = false)
-    @JoinColumn(name = "performed_by", nullable = false)
-    private Userentity performedBy;
-
-    @PrePersist
-    public void prePersist() {
-        if (eventDate == null) {
-            eventDate = LocalDateTime.now();
-        }
-    }
+    
+    
 
 
 }
