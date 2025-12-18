255c798b00186a4c59f44e14c874b04bf93982f5
bala-amypo <bala@amypo.in>
2025-12-18 09:46:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/LifecycleEvententity.java
+++ src/main/java/com/example/demo/entity/LifecycleEvententity.java
@@ -1,8 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,8 +26,60 @@ public class LifecycleEventEntity {
         this.id=id;
         this.asset=asset;
         this.eventType=eventType;
-        this.eventDescription=
+        this.eventDescription=eventDescription;
+        this.eventDate=eventDate;
+        this.performedBy=performedBy;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAsset() {
+        return asset;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public String getPerformedBy() {
+        return performedBy;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAsset(String asset) {
+        this.asset = asset;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setPerformedBy(String performedBy) {
+        this.performedBy = performedBy;
+    }
+
+    
   
     
     
