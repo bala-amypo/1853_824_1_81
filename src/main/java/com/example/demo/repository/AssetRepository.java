17fd3774e5838c55e450b01dbffed6a65dc3fa1e
bala-amypo <bala@amypo.in>
2025-12-19 09:23:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetRepository.java
+++ src/main/java/com/example/demo/repository/AssetRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Assetrepository extends JpaRepository<Assetrepository, Long> {
+public interface Assetrepository extends JpaRepository<Asset, Long> {
  
 }
