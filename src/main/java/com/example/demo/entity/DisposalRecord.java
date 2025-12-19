244af32690e124d2e8e2efbecbe271f2dcfd1c27
bala-amypo <bala@amypo.in>
2025-12-19 06:29:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/DisposalRecord.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+
+
+@Entity
+public class DisposalRecord{
+
+
+public DisposalRecord(){
+
+}
+
+public DisposalRecord(Long id,Strig asset,String disposalMethod,String disposalDate,String app)
+
+}
