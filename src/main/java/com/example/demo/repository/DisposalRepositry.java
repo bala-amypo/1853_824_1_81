f6fd0facb3cb8663d5f03e95ca03806bd69b5c08
bala-amypo <bala@amypo.in>
2025-12-20 09:53:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/DisposalRepositry.java
+++ src/main/java/com/example/demo/repository/DisposalRepositry.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.DiposalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LifecycleEventRepository extends JpaRepository<LifecycleEvent, Long> {
+public interface DisposalRecordRepositry extends JpaRepository<DiposalRecord, Long> {
  
 
 }
